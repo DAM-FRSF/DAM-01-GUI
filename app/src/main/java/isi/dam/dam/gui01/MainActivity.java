fbd88e6961b2e425373a95d1846dba0d47c3d81d
mdomingu <mdomingu@gmailcom>
2020-08-28 19:55:40 -0300

    Ejemplos de clase  28  Agosto.

--- app/src/main/java/isi/dam/dam/gui01/MainActivity.java
+++ app/src/main/java/isi/dam/dam/gui01/MainActivity.java
@@ -8,11 +8,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -22,7 +27,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageView imgCabecera;
     CheckBox cbOptA,cbOptB;
     TextView opcionesElegidas;
+    Switch sw1;
+    ToggleButton tg1;
     String estadoA,estadoB;
+    RadioGroup rg1,rg2;
+    RadioButton rb1,rb2,rb3,rb4;
+    EditText texto2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +46,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cbOptA = findViewById(R.id.optA);
         cbOptB = findViewById(R.id.optB);
         opcionesElegidas = findViewById(R.id.tvDescOpc);
+        sw1 = findViewById(R.id.switch1);
+        tg1 = findViewById(R.id.toggleButton1);
+        rg1 = findViewById(R.id.rg1);
+        rg2 = findViewById(R.id.rg2);
+        rb1 = findViewById(R.id.radioButton1);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
+        rb4 = findViewById(R.id.radioButton4);
+        texto2 = findViewById(R.id.texto2);
 
         if(cbOptA.isChecked()){
             estadoA ="OPCION A ELEGIDA ! ";
@@ -62,6 +81,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         if(isChecked) estadoB = "<<B>> ELEGIDO";
                         else estadoB = "<<B>> NOOO ELEGIDO";
                         break;
+                    case R.id.toggleButton1:
+                        break;
+                    case R.id.switch1:
+                        sw1.setText(isChecked? "ACTIVADO ! ": "NO ACTIVO !?!");
+                        break;
                 }
                 opcionesElegidas.setText(estadoA+" "+estadoB);
             }
@@ -69,6 +93,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         cbOptA.setOnCheckedChangeListener(listenerCB);
         cbOptB.setOnCheckedChangeListener(listenerCB);
+        sw1.setOnCheckedChangeListener(listenerCB);
+        tg1.setOnCheckedChangeListener(listenerCB);
+
+        // listener de RadioButton
+        RadioGroup.OnCheckedChangeListener radioListenerRG1 = new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButton1:
+                        //rb3.toggle();
+                        texto2.requestFocus();
+                        boton1.setEnabled(false);
+                        break;
+                    case R.id.radioButton2:
+                        rg2.clearCheck();
+                        boton1.setEnabled(true);
+
+                        break;
+                }
+            }
+
+
+        };
+
+            RadioGroup.OnCheckedChangeListener radioListenerRG2 = new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    switch (checkedId) {
+                        case R.id.radioButton3:
+                        case R.id.radioButton4:
+                    }
+                }
+
+
+            };
+
+        rg1.setOnCheckedChangeListener(radioListenerRG1);
+        rg2.setOnCheckedChangeListener(radioListenerRG2);
 
         boton1.setOnClickListener(new View.OnClickListener() {
             @Override
